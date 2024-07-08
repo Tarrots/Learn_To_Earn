a49230409d4fe4aa0fee8f12eafb0e8abdbf4489
Tarrots <thanhphong.tarrots@gmail.com>
2024-07-08 08:51:33 +0700

    init

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Spring Boot
+### `Libs`
+- Spring Web
+- H2 Database
+- Spring Data JPA
+- Oracle Driver
+### `Member`
+- Trần Thanh Phong
---
+++ src/test/java/com/example/spring/boot/learn/to/earn/LearnToEarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.learn.to.earn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnToEarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
